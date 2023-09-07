27213c4aa043a2094021fd5f049575e49db3d4d0
Abissa <a.sani@alustudent.com>
2023-09-07 13:53:46 -0700

    :robot: automated commit of 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fprintf(stderr,"%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "%s", "and that piece of art is useful\""
+		" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
