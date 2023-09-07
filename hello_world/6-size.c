df823a591673623d5e4e5499b656fe0b75139036
Abissa <a.sani@alustudent.com>
2023-09-07 13:30:36 -0700

    :robot: automated commit of one or many files

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,5 +12,8 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	int n;
+	n = 2;
+	printf("Size of n is: %lu\n", sizeof(n));
 	return (0);
 }
---
+++ hello_world/third.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	printf("Enter your age!\n");
+	scanf("%d", &n);
+	printf("Your age is %d\n", n);
+	return (0);
+}
