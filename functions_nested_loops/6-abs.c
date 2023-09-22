b46483cbe907d82c690c61c43f70c60aa3a551fa
Abissa <a.sani@alustudent.com>
2023-09-22 13:33:22 -0700

    int _abs(int);

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @n: int
+ *
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
