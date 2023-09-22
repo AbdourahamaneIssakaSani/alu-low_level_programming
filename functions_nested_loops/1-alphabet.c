717a4ee3e681acd865d44345f82c646932ad663b
Abissa <a.sani@alustudent.com>
2023-09-22 10:31:06 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+    char character = 'a';
+
+    while (character <= 'z')
+    {
+        _putchar(character);
+        character++;
+    }
+    _putchar(10);
+}
