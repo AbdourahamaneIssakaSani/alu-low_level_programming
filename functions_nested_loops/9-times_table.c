27d0706e89b4e7fb045add6cf44e6cda2480e1c0
Abissa <a.sani@alustudent.com>
2023-09-23 03:58:21 -0700

    boring betty

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,38 +9,38 @@
  */
 void times_table(void)
 {
-    int row = 0;
-    int column = 0;
-    int product = 0;
+	int row = 0;
+	int column = 0;
+	int product = 0;
 
-    while (row < 10)
-    {
-        column = 0;
-        while (column < 10)
-        {
-            product = row * column;
-            if (product < 10)
-            {
-                if (column != 0)
-                {
-                    _putchar(' ');
-                }
-                _putchar('0' + product);
-            }
-            else
-            {
-                _putchar('0' + product / 10);
-                _putchar('0' + product % 10);
-            }
+	while (row < 10)
+	{
+		column = 0;
+		while (column < 10)
+		{
+			product = row * column;
+			if (product < 10)
+			{
+				if (column != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar('0' + product);
+			}
+			else
+			{
+				_putchar('0' + product / 10);
+				_putchar('0' + product % 10);
+			}
 
-            if (column != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            column++;
-        }
-        _putchar('\n');
-        row++;
-    }
+			if (column != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			column++;
+		}
+		_putchar('\n');
+		row++;
+	}
 }
