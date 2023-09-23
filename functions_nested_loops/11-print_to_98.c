ad37e3f35f7dd5e87028d215aa9e80da9eb08b61
Abissa <a.sani@alustudent.com>
2023-09-23 04:04:56 -0700

    boring betty

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - prints all natural numbers from n to 98
  *
  * Return: Always 0 (Success)
  */
