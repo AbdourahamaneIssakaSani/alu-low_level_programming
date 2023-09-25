49ae2605c5058e8d44627bcd70ee3a2c9f24dacf
Abissa <a.sani@alustudent.com>
2023-09-25 14:03:02 -0700

    :robot: automated commit of one or many files

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: char
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+    int i = 0;
+    /* count string len */
+    while (*(s + i))
+    {
+        i++;
+    }
+
+    /*Remove the \0 count*/
+    i--;
+
+    while (i >= 0)
+    {
+        _putchar(*(s + i));
+        i--;
+    }
+
+    _putchar('\n');
+}
