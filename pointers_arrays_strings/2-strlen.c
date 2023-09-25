b047e38ed8ed4eae0fab7f0ef0ef7660a5f72775
Abissa <a.sani@alustudent.com>
2023-09-25 13:16:25 -0700

    :robot: automated commit of one or many files

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: char
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+    int count = 0;
+
+    while (*(s + count))
+    {
+        count++;
+    }
+    return (count);
+}
