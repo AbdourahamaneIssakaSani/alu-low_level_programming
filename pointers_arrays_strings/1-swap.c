ba6e9def14d0a986651ba07ecb877facdbdb9a1f
Abissa <a.sani@alustudent.com>
2023-09-25 13:28:18 -0700

    :robot: automated commit of 1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: int
+ * @b: int
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+    int c = *a;
+
+    *a = *b;
+    *b = c;
+}
