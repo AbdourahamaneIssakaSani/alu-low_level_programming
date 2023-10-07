7a3b406f187384daf717864299121d8d71a67a0c
Abissa <a.sani@alustudent.com>
2023-10-07 13:38:40 -0700

    :robot: automated commit of 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,41 +1,30 @@
-#include "main.h"
-
-int sqrt_search(int n, int low, int high);
+int find_sqrt(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to get the square root of
+ * @n: the number to find the square root of
  *
- * Return: the natural square root, or -1 if n has no natural square root
+ * Return: the natural square root of n or -1 if n doesn't have one
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    return sqrt_search(n, 0, n);
+	if (n < 0)
+		return (-1);
+	return (find_sqrt(n, 0));
 }
 
 /**
- * sqrt_search - helper function to search for the square root recursively
- * @n: the number to find the square root for
- * @low: lower limit of the search
- * @high: upper limit of the search
+ * find_sqrt - find the square root recursively
+ * @n: the number to find the square root of
+ * @i: the current divisor
  *
- * Return: the square root or -1 if there isn't one
+ * Return: the square root of n or -1 if none exists
  */
-int sqrt_search(int n, int low, int high)
+int find_sqrt(int n, int i)
 {
-    if (low > high)
-        return (-1);
-
-    int mid = (low + high) / 2;
-    int mid_squared = mid * mid;
-
-    if (mid_squared == n)
-        return mid;
-
-    if (mid_squared < n)
-        return sqrt_search(n, mid + 1, high);
-
-    return sqrt_search(n, low, mid - 1);
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (find_sqrt(n, i + 1));
 }
