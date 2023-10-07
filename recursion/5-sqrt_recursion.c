6b6dbc7d6199e33801cb41c714878407c5ccb115
Abissa <a.sani@alustudent.com>
2023-10-07 13:32:33 -0700

    betty hacked

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,41 @@
 #include "main.h"
 
+int sqrt_search(int n, int low, int high);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to find square root of
+ * @n: the number to get the square root of
  *
- * Return: square root of n
+ * Return: the natural square root, or -1 if n has no natural square root
  */
 int _sqrt_recursion(int n)
 {
-    return (helper(n, 1));
+    if (n < 0)
+        return (-1);
+    return sqrt_search(n, 0, n);
+}
+
+/**
+ * sqrt_search - helper function to search for the square root recursively
+ * @n: the number to find the square root for
+ * @low: lower limit of the search
+ * @high: upper limit of the search
+ *
+ * Return: the square root or -1 if there isn't one
+ */
+int sqrt_search(int n, int low, int high)
+{
+    if (low > high)
+        return (-1);
+
+    int mid = (low + high) / 2;
+    int mid_squared = mid * mid;
+
+    if (mid_squared == n)
+        return mid;
+
+    if (mid_squared < n)
+        return sqrt_search(n, mid + 1, high);
+
+    return sqrt_search(n, low, mid - 1);
 }
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,12 +1,30 @@
-#include "main.h"
+int is_divisible(int n, int i);
 
 /**
  * is_prime_number - checks if a number is prime
- * @n: number to check
+ * @n: the number to check
  *
  * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-    return (helper(n, 2));
+    if (n <= 1)
+        return (0);
+    return (is_divisible(n, 2));
+}
+
+/**
+ * is_divisible - checks if a number is divisible
+ * @n: the number to check
+ * @i: the current divisor
+ *
+ * Return: 0 if n is divisible by an integer from 2 to n/2, 1 otherwise
+ */
+int is_divisible(int n, int i)
+{
+    if (i > n / 2)
+        return (1);
+    if (n % i == 0)
+        return (0);
+    return (is_divisible(n, i + 1));
 }
