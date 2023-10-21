135a95e04f05f0d54d139c7d3e1ced25b3e22c00
Abissa <a.sani@alustudent.com>
2023-10-21 00:10:46 -0700

    :robot: automated commit of one or many files

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - prints a struct dog
+* @d: pointer to struct dog
+*
+* Return: void
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
