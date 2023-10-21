7e7b920a15a4c9a2498e19a2f06e6c999502a3a7
Abissa <a.sani@alustudent.com>
2023-10-21 00:00:43 -0700

    :robot: automated commit of one or many files

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+/**
+ * struct dog - dog's information
+ * @name: dog's name
+ * @breed: dog's breed
+ * @age: dog's age
+ * @weight: dog's weight
+*/
+struct dog
+{
+	char name[50];
+
+	char breed[50];
+
+	int age;
+
+	double weight;
+
+};
+#endif
+
