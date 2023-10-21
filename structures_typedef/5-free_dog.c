b03e59d523b1a65476a984780500610bf16f1a6a
Abissa <a.sani@alustudent.com>
2023-10-21 00:16:08 -0700

    :robot: automated commit of one or many files

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - frees a dog
+* @d: pointer to dog to free
+*
+* Return: void
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free((*d).name);
+	free((*d).owner);
+	free(d);
+}
