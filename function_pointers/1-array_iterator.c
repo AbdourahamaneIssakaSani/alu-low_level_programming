c35f8382f46df5c05935d424ad11e0474b066c79
Abissa <a.sani@alustudent.com>
2023-10-23 01:30:54 -0700

    :robot: automated commit of 1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,21 +1,24 @@
 #include <stddef.h>
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
- * array_iterator - executes a function given as a parameter on each element
- * @array: array to iterate through
- * @size: size of array
- * @action: function pointer
- *
- * Return: void
- */
+* array_iterator - executes a function given as a parameter on each element
+* @array: array to iterate through
+* @size: size of array
+* @action: function pointer
+*
+* Return: void
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	if (action == NULL)
+		return;
 	size_t temp = 0;
 
 	while (temp < size)
 	{
-		_putchar(array[temp]);
+		(*action)(array[temp]);
 		temp++;
 	}
 }
