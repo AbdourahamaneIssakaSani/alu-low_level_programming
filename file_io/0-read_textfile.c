fe1001c22a7cd895846604ae370dd8c53f1446da
Abissa <a.sani@alustudent.com>
2023-11-06 01:19:47 -0800

    :robot: automated commit of 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,64 +1,102 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
- *
- * Return: actual number of letters it could read and print
- */
+* open_file - opens a file and returns the file descriptor
+* @filename: name of the file to open
+*
+* Return: file descriptor on success, -1 on failure
+*/
+int open_file(const char *filename)
+{
+	int fd = open(filename, O_RDONLY);
+
+	return (fd);
+}
+
+/**
+* read_file - reads from a file descriptor into a buffer
+* @fd: file descriptor
+* @buffer: buffer to store the data
+* @letters: number of letters to read
+*
+* Return: number of bytes read on success, -1 on failure
+*/
+ssize_t read_file(int fd, char *buffer, size_t letters)
+{
+	ssize_t n_read = read(fd, buffer, letters);
+	return (n_read);
+}
+
+/**
+* write_buffer - writes a buffer to standard output
+* @buffer: buffer containing data
+* @n_read: number of bytes to write
+*
+* Return: number of bytes written on success, -1 on failure
+*/
+ssize_t write_buffer(char *buffer, ssize_t n_read)
+{
+	ssize_t n_written = write(STDOUT_FILENO, buffer, n_read);
+	return (n_written);
+}
+
+/**
+* close_file - closes a file descriptor
+* @fd: file descriptor to close
+*/
+void close_file(int fd)
+{
+	close(fd);
+}
+
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output
+* @filename: name of the file to read
+* @letters: number of letters it should read and print
+*
+* Return: actual number of letters it could read and print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/* File descriptor */
 	int fd;
 
-	/* Number of letters it could read and print */
 	ssize_t n_read, n_written;
-
-	/* Buffer */
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	/* Allocate memory for the buffer */
 	buffer = malloc(letters);
 	if (buffer == NULL)
 		return (0);
 
-	/* Open the file */
-	fd = open(filename, O_RDONLY);
+	fd = open_file(filename);
 
-	/* Check if the file can be opened */
 	if (fd == -1)
 	{
 		free(buffer);
 		return (0);
 	}
 
-	/* Read from the file */
-	n_read = read(fd, buffer, letters);
+	n_read = read_file(fd, buffer, letters);
 
-	/* Check if the file can be read */
 	if (n_read == -1)
 	{
 		free(buffer);
-		close(fd);
+		close_file(fd);
 		return (0);
 	}
 
-	/* Write to standard output */
-	n_written = write(STDOUT_FILENO, buffer, n_read);
+	n_written = write_buffer(buffer, n_read);
 
-	/* Check if the file can be written */
 	if (n_written == -1 || n_written != n_read)
 	{
 		free(buffer);
-		close(fd);
+		close_file(fd);
 		return (0);
 	}
 
-	close(fd);
+	close_file(fd);
 	free(buffer);
 
 	return (n_read);
