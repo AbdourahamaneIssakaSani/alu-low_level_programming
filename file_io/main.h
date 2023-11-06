b6aaa3f5f9ba6896337988977db319bfc292a0a0
Abissa <a.sani@alustudent.com>
2023-11-06 00:56:48 -0800

    :robot: automated commit of main.h

---
+++ file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE_IO
+#define HEADER_FILE_IO
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/*Prototypes*/
+ssize_t read_textfile(const char *filename, size_t letters);
+
+int create_file(const char *filename, char *text_content);
+
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /*HEADER_FILE_IO*/
