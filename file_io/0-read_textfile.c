6293703416d32d1b558a9396fc47f148b45c7329
Abissa <a.sani@alustudent.com>
2023-11-06 01:11:41 -0800

    :robot: automated commit of one or many files

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,52 +9,52 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;  /* File descriptor */
-    ssize_t n_read, n_written;  /* Number of letters it could read and print */
-    char *buffer;  /* Buffer */
+	int fd;  /* File descriptor */
+	ssize_t n_read, n_written;  /* Number of letters it could read and print */
+	char *buffer;  /* Buffer */
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    /* Allocate memory for the buffer */
-    buffer = malloc(letters);
-    if (buffer == NULL)
-        return (0);
+	/* Allocate memory for the buffer */
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
 
-    /* Open the file */
-    fd = open(filename, O_RDONLY);
+	/* Open the file */
+	fd = open(filename, O_RDONLY);
 
-    /* Check if the file can be opened */
-    if (fd == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	/* Check if the file can be opened */
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    /* Read from the file */
-    n_read = read(fd, buffer, letters);
+	/* Read from the file */
+	n_read = read(fd, buffer, letters);
 
-    /* Check if the file can be read */
-    if (n_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	/* Check if the file can be read */
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    /* Write to standard output */
-    n_written = write(STDOUT_FILENO, buffer, n_read);
+	/* Write to standard output */
+	n_written = write(STDOUT_FILENO, buffer, n_read);
 
-    /* Check if the file can be written */
-    if (n_written == -1 || n_written != n_read)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	/* Check if the file can be written */
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    close(fd);
-    free(buffer);
+	close(fd);
+	free(buffer);
 
-    return (n_read);
+	return (n_read);
 }
