cde8f7a433e9a7f90864ac8cd1f530b4f44b1f32
Abissa <a.sani@alustudent.com>
2023-11-06 01:59:08 -0800

    :robot: automated commit of 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,64 +1,52 @@
 #include "main.h"
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: array of pointers to the arguments
- *
- * Return: 0 on success
- */
+* main - copies the content of a file to another file
+* @argc: number of arguments
+* @argv: array of pointers to the arguments
+*
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to;  /* File descriptors */
-    ssize_t n_read, n_written;  /* Number of letters it could read and print */
-    char *buffer;  /* Buffer */
+	int fd_from, fd_to; /* File descriptors */
 
-    /* Check if the number of arguments is correct */
-    if (argc != 3)
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	ssize_t n_read, n_written; /* Number of letters it could read and print */
+	char buffer[1024];         /* Buffer */
 
-    /* Allocate memory for the buffer */
-    buffer = malloc(1024);
-    if (buffer == NULL)
-        return (0);
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-    /* Open the file_from */
-    fd_from = open(argv[1], O_RDONLY);
+	/* Open the file_from */
+	fd_from = open(argv[1], O_RDONLY);
 
-    /* Check if the file_from can be opened */
-    if (fd_from == -1)
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    /* Open the file_to */
-    fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	/* Open the file_to */
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-    /* Check if the file_to can be opened */
-    if (fd_to == -1)
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	if (fd_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
-    /* Read from the file_from */
-    n_read = read(fd_from, buffer, 1024);
+	while ((n_read = read(fd_from, buffer, 1024)) > 0)
+	{
+		n_written = write(fd_to, buffer, n_read);
+		if (n_written == -1 || n_written != n_read)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
 
-    /* Check if the file_from can be read */
-    if (n_read == -1)
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    /* Write to the file_to */
-    n_written = write(fd_to, buffer, n_read);
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 
-    /* Check if the file_to can be written */
-    if (n_written == -1 || n_written != n_read)
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-
-    /* Close the file_from */
-    if (close(fd_from) == -1)
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-
-    /* Close the file_to */
-    if (close(fd_to) == -1)
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-
-    free(buffer);
-
-    return (0);
+	return (0);
 }
